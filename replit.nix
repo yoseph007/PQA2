91abf921dae293a51d68056d9b49b65fe7222c3e
yoseph007 <yos@sky.com>
2025-03-31 20:34:41 +0000

    Update UI layuout and file org with timestamp

--- replit.nix
+++ replit.nix
@@ -1,5 +1,16 @@
 {pkgs}: {
   deps = [
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.ffmpeg-full
+    pkgs.cairo
+    pkgs.freetype
+    pkgs.glibcLocales
     pkgs.libGLU
     pkgs.libGL
   ];
